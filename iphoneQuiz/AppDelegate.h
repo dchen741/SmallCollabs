b5cb3730af67963d986b5b6da6cefdec1b475af3
Ivan Seto <ivanseto@Ivans-Mac.local>
2014-07-13 12:18:32 -0700

    Initial Commit

---
+++ iphoneQuiz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iphoneQuiz
+//
+//  Created by Ivan Seto on 7/13/14.
+//  Copyright (c) 2014 Ivan Seto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iphoneQuiz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iphoneQuiz
+//
+//  Created by Ivan Seto on 7/13/14.
+//  Copyright (c) 2014 Ivan Seto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
